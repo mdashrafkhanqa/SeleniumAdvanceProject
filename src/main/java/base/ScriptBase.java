dccb2de9e43ef258feb7c5d32550c62bdee30ab5
ashraf <mdashrafkhanqa@gmail.com>
2020-05-01 21:55:25 -0400

    OpenBrowser branch created and open browser by create @Test annotation

--- src/main/java/base/ScriptBase.java
+++ src/main/java/base/ScriptBase.java
@@ -1,5 +1,17 @@
 package base;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
 public class ScriptBase {
-    
+
+    public WebDriver driver;
+
+    @Test
+    public void beforeTest(){
+        System.setProperty("webdriver.chrome.driver","./drivers/chromedriver");
+        driver = new ChromeDriver();
+        driver.get("http://automationpractice.com/index.php");
+    }
 }
